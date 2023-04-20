ea1c03f4f98a11dbed5928bed4f764e1457bf1de
h311x <amirtadzhiev@gmail.com>
2023-04-20 21:01:41 +0400

    feat: playlists layout

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,31 @@
 <script setup lang="ts">
 import LikesPage from './components/LikesPage.vue'
-import { ArrowPathIcon } from '@heroicons/vue/24/solid'
+import { ArrowPathIcon, HeartIcon, QueueListIcon } from '@heroicons/vue/24/solid'
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from './components/ui/Tabs'
 </script>
 
 <template>
   <Suspense>
-    <LikesPage />
+    <div class="flex">
+      <TabGroup vertical>
+        <TabList class="h-screen flex flex-col !justify-start">
+          <Tab>
+            <HeartIcon class="w-6 h-6" />
+          </Tab>
+          <Tab>
+            <QueueListIcon class="w-6 h-6" />
+          </Tab>
+        </TabList>
 
+        <TabPanels class="grow">
+          <TabPanel>
+            <LikesPage />
+          </TabPanel>
+
+          <TabPanel> `Yo </TabPanel>
+        </TabPanels>
+      </TabGroup>
+    </div>
     <template #fallback>
       <div class="w-screen h-screen flex items-center justify-center">
         <ArrowPathIcon class="w-8 h-8 animate-spin" />
--- src/components/ui/Tabs.tsx
+++ src/components/ui/Tabs.tsx
@@ -11,21 +11,23 @@ type CompTypes<Comp> = Comp extends Component<infer P> ? P : never
 
 const TabGroup = HuiTabGroup
 const TabPanels = HuiTabPanels
-const TabList: FunctionalComponent<CompTypes<typeof HuiTabList>> = (props, { slots }) => {
+const TabList: FunctionalComponent<CompTypes<typeof HuiTabList>> = (props, { slots, attrs }) => {
   return (
     <HuiTabList
       class="inline-flex h-10 items-center justify-center rounded-md bg-muted p-1 text-muted-foreground"
       {...props}
+      {...attrs}
     >
       {slots.default?.()}
     </HuiTabList>
   )
 }
-const Tab: FunctionalComponent<CompTypes<typeof HuiTab>> = (props, { slots }) => {
+const Tab: FunctionalComponent<CompTypes<typeof HuiTab>> = (props, { slots, attrs }) => {
   return (
     <HuiTab
       class="inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[headlessui-state=selected]:bg-background data-[headlessui-state=selected]:text-foreground data-[headlessui-state=selected]:shadow-sm"
       {...props}
+      {...attrs}
     >
       {slots.default?.()}
     </HuiTab>
@@ -35,7 +37,7 @@ const Tab: FunctionalComponent<CompTypes<typeof HuiTab>> = (props, { slots }) =>
 const TabPanel: FunctionalComponent<CompTypes<typeof HuiTabPanel>> = (props, { slots }) => {
   return (
     <HuiTabPanel
-      class="mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2"
+      class="ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2"
       {...props}
     >
       {slots.default?.()}
